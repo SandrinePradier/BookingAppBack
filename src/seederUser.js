ac350bb355a0e52a1ea454eca606d9aaeb16cf56
Sandrine Pradier <labelleaffranchie@gmail.com>
2018-06-06 09:02:25 +0200

    Feature | add seederUser.js

---
+++ src/seederUser.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import User from './modules/professional/auth/modelauth.js'
+
+
+mongoose.connect('mongodb://localhost:27017/bookingappDB', (err) => {
+	if (err){throw err;}
+	else{
+		console.log('the data base is connected');
+
+
+	}
+})
+
+let user = new User({
+	username: 'pro@gmail.com',
+	password: '123',
+})
+
+user.save(function(err, result){
+	if (err){
+		console.log('error');
+	}
+	else{
+		console.log('Seeder user saved');
+		mongoose.disconnect();
+	}
+})
+
