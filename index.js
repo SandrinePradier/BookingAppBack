1b68dd08f14965a5218d4c193581ecc47270b819
Sandrine Pradier <labelleaffranchie@gmail.com>
2018-06-07 19:21:47 +0200

    test

--- index.js
+++ index.js
@@ -31,8 +31,9 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 
 // CORS cross-origin
 app.use(function (req, res, next) {
- res.header(`Access-Control-Allow-Origin`, `https://mybookingapppro.herokuapp.com`);
- res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+ // res.header(`Access-Control-Allow-Origin`, `https://mybookingapppro.herokuapp.com`);
+ res.header(`Access-Control-Allow-Origin`, `*`);
+ res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE, OPTIONS`);
  res.header(`Access-Control-Allow-Headers`, `Origin, X-Requested-With, Content-Type, Accept, token`);
  // res.header(`Access-Control-Expose-Headers`,  `token`);
  // intercept OPTIONS method
