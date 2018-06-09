3b20aada11a1d0df914ddbf32ec1b19c2e8a8717
Sandrine Pradier <labelleaffranchie@gmail.com>
2018-06-09 09:37:05 +0200

    init branch dev

---
+++ env_prod.js
@@ -0,0 +1,5 @@
+SECRETKEY=hola
+PORT=2707
+DB=mongodb://pradiersandrine:mybookingapp1@ds247290.mlab.com:47290/db_mybookingapp
+
+//il suffit de faire un cp coller de ça ds le .env pour repasser en prod
